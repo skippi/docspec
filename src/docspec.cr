552bb7787afd31218c573dff7f78d1dd687d13ce
Justin <jbtcao@gmail.com>
2018-06-01 23:11:00 -0500

    Added doctest annotation

--- src/docspec.cr
+++ src/docspec.cr
@@ -1,26 +1,31 @@
 require "./docspec/*"
 
 module Docspec
-  DOCTEST_PREFIX        = /^>>/
-  DOCTEST_RESULT_PREFIX = /# =>/
-  COMMENT_REGEX = /^\s*#/
-  CODEBLOCK_REGEX = /^\s*#\s*```/
+  DOCTEST_PREFIX           = /^>>/
+  DOCTEST_RESULT_PREFIX    = /# =>/
+  COMMENT_REGEX            = /^\s*#/
+  CODEBLOCK_REGEX          = /^\s*#\s*```/
+  DOCTEST_ANNOTATION_REGEX = /^\s*#\s?\[\]\(@doctest\)/
 
   # Parses *filename* for marked examples to create specs.
   macro doctest(filename)
     {% calling_dir = filename.filename.gsub(/[^\/]*$/, "") %}\
-    {% parser = {mode: :default} %}\
+    {% parser = {mode: :default, doctest_all: false} %}\
     {% for line, index in `cd #{calling_dir} && cat #{filename}`.lines %}\
       {% if line =~ Docspec::CODEBLOCK_REGEX %}\
         {% parser[:mode] = (parser[:mode] == :codeblock) ? :default : :codeblock %}\
+      {% elsif line =~ Docspec::DOCTEST_ANNOTATION_REGEX %}\
+        {% parser[:doctest_all] = true %}\
       {% elsif line =~ Docspec::COMMENT_REGEX %}\
         {% if parser[:mode] == :codeblock %}\
-          Docspec.doctest_code_line({{line.strip}}, {{filename}}, {{index + 1}})
+          Docspec.doctest_code_line({{line.strip}}, {{filename}}, {{index + 1}}, {{parser}})
         {% else %}\
+          {% parser[:doctest_all] = false %}\
           Docspec.doctest_comment({{line.strip}}, {{filename}}, {{index + 1}})
         {% end %}\
       {% elsif !line.strip.empty? %}\
         {% parser[:mode] = :default %}\
+        {% parser[:doctest_all] = false %}\
       {% end %}\
     {% end %}\
   end
@@ -35,14 +40,14 @@ module Docspec
   end
 
   # :nodoc:
-  macro doctest_code_line(line, filename, row)
+  macro doctest_code_line(line, filename, row, parser)
     {% no_comment_line = line.strip.gsub(/^#/, "") %}\
-    Docspec.doctest_example({{no_comment_line}}, {{filename}}, {{row}})
+    Docspec.doctest_example({{no_comment_line}}, {{filename}}, {{row}}, {{parser}})
   end
 
   # :nodoc:
-  macro doctest_example(line, filename, row)
-    {% if line.strip =~ Docspec::DOCTEST_PREFIX %}\
+  macro doctest_example(line, filename, row, parser)
+    {% if parser[:doctest_all] || line.strip =~ Docspec::DOCTEST_PREFIX %}\
       Docspec.doctest_marked_example({{line}}, {{filename}}, {{row}})
     {% end %}\
   end
